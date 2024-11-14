0c6df245338b4f240cc983481e35914ff2c1a7a1
Uber020 <renanhs1@hotmail.com>
2024-11-14 02:17:26 -0300

    test comment

--- kojima/src/App.jsx
+++ kojima/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     return (
         <BrowserRouter>
             <div className="App">
+                {/*comment*/}
                 <Navbar />
                 <ThemeToggle />
                 <Routes>
